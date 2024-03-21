599f2e81eefa0f3cbf7cb91260a0b7e74f6a19b7
Anderson <andersonribeirodemorais@gmail.com>
2024-03-21 16:05:44 -0300

    Signed-off-by: Anderson <andersonribeirodemorais@gmail.com>

---
+++ diagrama classe jogo/Level.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window Surface = None
+        self.entity_list str = None
+        self.name_str = None
+
+    def run(self, ):
+        pass
+
